75da1ad727b82d08b909559caece2eed59c49566
SanzinaHossain <sanzina67hossain@gmail.com>
2023-02-17 12:38:47 +0600

    home edit

--- src/Pages/Services/ContactDetails.js
+++ src/Pages/Services/ContactDetails.js
@@ -1,43 +1,67 @@
-import React from 'react'
-import p1 from "../../image/services/address.jpg"
-import p2 from "../../image/services/phone.jpg"
-import p3 from "../../image/services/hour.jpg"
-import p4 from "../../image/services/hour.webp"
+import React from "react";
+import p1 from "../../image/services/address.jpg";
+import p2 from "../../image/services/phone.jpg";
+import p3 from "../../image/services/hour.jpg";
+import p4 from "../../image/services/hour.webp";
 const ContactDetails = () => {
   return (
-   <div class="mt-16 mb-10">
-        <h1 class="text-orange-800 text-3xl text-center font-bold">OUR CONTACT INFO</h1>
-        <h1 class="text-black text-lg text-center">We do all the work, you get all the credit</h1>
-        <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 lg:mx-12 mx-8 mt-8">
-       <div class="w-64 h-80 p-7 pt-12 ">
-         <img class=" pl-1 pr-1 rounded-full border-2 border-gray-300 h-28 mx-auto w-28" src={p1}></img>
+    <div class="pt-16 pb-10 bg-white">
+      <h1 class="text-orange-800 text-3xl text-center font-bold">
+        OUR CONTACT INFO
+      </h1>
+      <h1 class="text-black text-lg text-center">
+        We do all the work, you get all the credit
+      </h1>
+      <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 lg:mx-12 mx-8 mt-8">
+        <div class="w-64 h-80 p-7 pt-12 ">
+          <img
+            class=" pl-1 pr-1 rounded-full border-2 border-gray-300 h-28 mx-auto w-28"
+            src={p1}
+          ></img>
           <h1 class="text-2xl text-center text-orange-600 pt-4">ADDRESS</h1>
-          <h1 class="text-lg text-gray-700 text-center pt-3">Reef Academy,12-14 Kensington High Street London, UK</h1>
-       </div>
-       <div class="w-64 h-80  pt-12 ">
-          <img class="pl-1 pr-1 rounded-full border-2 border-gray-300 h-28 w-28 mx-auto" src={p2}></img>
-          <h1 class="text-2xl text-center text-orange-600 pt-4">PHONE & EMAIL</h1>
+          <h1 class="text-lg text-gray-700 text-center pt-3">
+            Reef Academy,12-14 Kensington High Street London, UK
+          </h1>
+        </div>
+        <div class="w-64 h-80  pt-12 ">
+          <img
+            class="pl-1 pr-1 rounded-full border-2 border-gray-300 h-28 w-28 mx-auto"
+            src={p2}
+          ></img>
+          <h1 class="text-2xl text-center text-orange-600 pt-4">
+            PHONE & EMAIL
+          </h1>
           <h1 class="text-lg text-gray-700 text-center pt-3">+123456 7890</h1>
           <h1 class="text-lg text-gray-700 text-center">0987654321</h1>
-          <h1 class="text-lg text-gray-700 text-center ">contact@zoutula.com</h1>
-       </div>
-       <div class="w-64 h-80  p-7  pt-12 ">
-          <img class=" pl-1 pr-1 h-28 mx-auto rounded-full border-2 border-gray-300 w-28" src={p3}></img>
-          <h1 class=" text-2xl text-center text-orange-600 pt-4">BUSINESS HOURS</h1>
+          <h1 class="text-lg text-gray-700 text-center ">
+            contact@zoutula.com
+          </h1>
+        </div>
+        <div class="w-64 h-80  p-7  pt-12 ">
+          <img
+            class=" pl-1 pr-1 h-28 mx-auto rounded-full border-2 border-gray-300 w-28"
+            src={p3}
+          ></img>
+          <h1 class=" text-2xl text-center text-orange-600 pt-4">
+            BUSINESS HOURS
+          </h1>
           <h1 class="text-lg text-gray-700 text-center pt-3">Monday-Friday</h1>
           <h1 class="text-lg text-gray-700 text-center">8.00 am – 6.00 pm</h1>
           <h1 class="text-lg text-gray-700 text-center ">Weekend Closed</h1>
-       </div>
-       <div class="w-64 h-80  p-7  pt-12 ">
-          <img class=" pl-1 pr-1 w-28 h-28 mx-auto rounded-full border-2 border-gray-300" src={p4}></img>
+        </div>
+        <div class="w-64 h-80  p-7  pt-12 ">
+          <img
+            class=" pl-1 pr-1 w-28 h-28 mx-auto rounded-full border-2 border-gray-300"
+            src={p4}
+          ></img>
           <h1 class="text-2xl text-center text-orange-600 pt-4">REEF HOURS</h1>
           <h1 class="text-lg text-gray-700 text-center pt-3">Monday-Friday</h1>
           <h1 class="text-lg text-gray-700 text-center">8.00 am – 6.00 pm</h1>
           <h1 class="text-lg text-gray-700 text-center ">Weekend Closed</h1>
-       </div>
+        </div>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
-export default ContactDetails
+export default ContactDetails;
--- src/Pages/Services/Enrollment.js
+++ src/Pages/Services/Enrollment.js
@@ -1,54 +1,78 @@
-import React from 'react'
-import ContactDetails from './ContactDetails'
+import React from "react";
+import ContactDetails from "./ContactDetails";
 const Enrollment = () => {
   return (
-    <div class="w-full h-auto p-12 mt-5">
-       <h1 class="text-orange-700 text-center text-4xl font-bold ">Enrollment</h1>
-       <h1 class="text-black text-center lg:text-xl text-sm">Steps done with heart, soul, mind & strength</h1>
-       <div class="mx-auto mt-9 lg:block hidden">
-       <ul class="steps steps-vertical lg:steps-horizontal ">
-  <li class="step step-success w-70 lg:px-12 px-0">
-    <h1 class="text-center text-black text-2xl">Application</h1>
-    <h1 class="lg:text-center text-justify text-black lg:pl-0 pl-20">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-  
-  </li>
-  <li class="step step-success w-70">
-    <h1 class="text-center text-black text-2xl">Counseling</h1>
-    <h1 class="text-center text-black px-12">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-  
-  </li>
-  <li class="step step-success w-70 ">
-    <h1 class="text-center text-black text-2xl">Admission</h1>
-    <h1 class="text-center text-black px-12">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-  
-  </li>
-  <li class="step step-success w-70 ">
-    <h1 class="text-center text-black text-2xl">Registration</h1>
-    <h1 class="text-center text-black px-12">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-  
-  </li>
-</ul>
-       </div>
-       <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 lg:mx-12 mx-8 mt-16 lg:hidden ">
-       <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
+    <div class="w-full h-auto p-12 pt-5 bg-white">
+      <h1 class="text-orange-700 text-center text-4xl font-bold ">
+        Enrollment
+      </h1>
+      <h1 class="text-black text-center lg:text-xl text-sm">
+        Steps done with heart, soul, mind & strength
+      </h1>
+      <div class="mx-auto mt-9 lg:block hidden">
+        <ul class="steps steps-vertical lg:steps-horizontal ">
+          <li class="step step-success w-70 lg:px-12 px-0">
+            <h1 class="text-center text-black text-2xl">Application</h1>
+            <h1 class="lg:text-center text-justify text-black lg:pl-0 pl-20">
+              Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+              vim enim rebum.
+            </h1>
+          </li>
+          <li class="step step-success w-70">
+            <h1 class="text-center text-black text-2xl">Counseling</h1>
+            <h1 class="text-center text-black px-12">
+              Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+              vim enim rebum.
+            </h1>
+          </li>
+          <li class="step step-success w-70 ">
+            <h1 class="text-center text-black text-2xl">Admission</h1>
+            <h1 class="text-center text-black px-12">
+              Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+              vim enim rebum.
+            </h1>
+          </li>
+          <li class="step step-success w-70 ">
+            <h1 class="text-center text-black text-2xl">Registration</h1>
+            <h1 class="text-center text-black px-12">
+              Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+              vim enim rebum.
+            </h1>
+          </li>
+        </ul>
+      </div>
+      <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 lg:mx-12 mx-8 mt-16 lg:hidden ">
+        <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
           <h1 class="text-2xl text-center text-black pt-4">Application</h1>
-          <h1 class="text-lg text-gray-700 text-center pt-3">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-       </div>
-       <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
+          <h1 class="text-lg text-gray-700 text-center pt-3">
+            Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+            vim enim rebum.
+          </h1>
+        </div>
+        <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
           <h1 class="text-2xl text-center text-black pt-4">Counseling</h1>
-          <h1 class="text-lg text-gray-700 text-center pt-3">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-       </div>
-       <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
+          <h1 class="text-lg text-gray-700 text-center pt-3">
+            Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+            vim enim rebum.
+          </h1>
+        </div>
+        <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
           <h1 class="text-2xl text-center text-black pt-4">Admission</h1>
-          <h1 class="text-lg text-gray-700 text-center pt-3">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-       </div>
-       <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
+          <h1 class="text-lg text-gray-700 text-center pt-3">
+            Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+            vim enim rebum.
+          </h1>
+        </div>
+        <div class="w-64 h-72 bg-white p-7 shadow-2xl pt-12 ">
           <h1 class="text-2xl text-center text-black pt-4">Registration</h1>
-          <h1 class="text-lg text-gray-700 text-center pt-3">Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne vim enim rebum.</h1>
-       </div>
+          <h1 class="text-lg text-gray-700 text-center pt-3">
+            Alterum accommodare duo cu. Ius labore luptatum efficiendi ex, ne
+            vim enim rebum.
+          </h1>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Enrollment
+export default Enrollment;
